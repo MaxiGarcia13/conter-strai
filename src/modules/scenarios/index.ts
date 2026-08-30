ea2003586ab56eb67b0263b275952628b7e5f571
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-08-30 13:44:19 +0200

    refactor: Remove RemotePlayer and ScenarioSoldiers components to streamline multiplayer and scenario management, enhancing modularity and performance

--- src/modules/scenarios/index.ts
+++ src/modules/scenarios/index.ts
@@ -1,5 +1,4 @@
 export * from './components/scenario-scene';
-export * from './components/scenario-soldiers';
 export * from './get-scenario-by-id';
 export * from './get-scenario-texture-ids';
 export * from './maps';
